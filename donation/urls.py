cc50009ce31495b994e23234347a9106b31aff02
Bimo Priyohutomo <bimopriyoh@gmail.com>
2022-10-28 13:15:04 +0700

    belum bagus tampilannya

--- donation/urls.py
+++ donation/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path
-from donation.views import index, show_json, transaksi_donasi, add_donasi
+from donation.views import donasi_saya, hapus_donasi, index, show_json, transaksi_donasi, add_donasi
 
 app_name = 'donation'
 
@@ -11,4 +11,6 @@ urlpatterns = [
     path('transaksi_donasi/<str:id>/', transaksi_donasi, name='transaksi_donasi'),
 
     path('json/', show_json, name='show_json'),
+    path('donasi_saya', donasi_saya, name='donasi_saya'),
+    path('hapus_donasi/<str:id>/', hapus_donasi, name='hapus_donasi')
 ]
